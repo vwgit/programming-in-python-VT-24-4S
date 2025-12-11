2b7e537a326bcc8778650893ace8b27cd512ee2b
delcortina <rikoshku@gmail.com>
2025-12-11 10:32:27 +0500

    рекурсия 5 вариант 2 задание

---
+++ 5varrec2task.py
@@ -0,0 +1,11 @@
+#5вар 2 задание(рекурсия)
+def collect_keys(d):
+	key = []
+	def help(table):
+		for key, value in table.items():
+			keys.append(key)
+			if isinstance(value,dict):
+				help(value)
+	help(d)
+	return keys
+print(collect_keys({"a" : 1,"b" : {"c" : 2, "d" : 3}}))
