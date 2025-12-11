ae9d3c9906bd9c13538b5f110a5f350953d9683e
delcortina <rikoshku@gmail.com>
2025-12-11 10:21:09 +0500

    рекурсия 5 вариант 1 задание

---
+++ 5varrec1task.py
@@ -0,0 +1,12 @@
+#5вар 1 задание
+import math
+def power(x, n):
+	if n == 0:
+		return 1
+	return x * power(x, n - 1)
+
+def rec_sum(x, n):
+	if n == 0:
+		return 0
+	return power(x, n) /(power(math.e, power(i, 2)) + rec_sum(x, n - 1)
+print(recursive_sum(2, 3)) 
